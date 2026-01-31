31ad4ebb9dbe6740aef82b37b4a1d124f4a29dd9
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2026-01-31 11:46:44 -0800

    Very crude stepping loop

--- rospovm/main.cpp
+++ rospovm/main.cpp
@@ -25,12 +25,19 @@ int main(int argc, char* argv[]) {
     for (const auto& segment : binary.segments) {
         std::cout << "Loading segment at address 0x" << std::hex << segment.address 
                   << " with size " << std::dec << segment.data.size() << " bytes." << std::endl;
+        for (uint8_t byte : segment.data) {
+            std::cout << std::hex << static_cast<int>(byte) << " ";
+        }
+        std::cout << std::dec << std::endl;
         vm.loadBinaryAtAddress(std::vector<char>(segment.data.begin(), segment.data.end()), segment.address);
     }
-    exit(0); // Temporary exit to avoid infinite loop during testing
+    std::cout << "Loaded. Starting execution..." << std::endl;
 
+    char ch;
     // Simple execution loop
     while (true){
+        std::cin.get(ch);
+        if (ch == 'q') break;
         vm.step();
     }
     
