77d4fa351233fe51485831c6225a5257c2a0690c
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2026-02-02 10:48:53 -0800

    Clean up project in preparation for start of OS code

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euvo pipefail
 source rospoas/venv/bin/activate
-python3 rospoas/compile.py --input rospoas/test.ros --output rospovm/test.rosp
+python3 rospoas/compile.py --input rospos/main.ros --output rospos/build/rospos.rosp
+hexdump -C rospos/build/rospos.rosp
 cd rospovm
 make -j8
-hexdump -C test.rosp
-./main ./test.rosp debug
+cd ..
+./rospovm/rospovm ./rospos/build/rospos.rosp debug
