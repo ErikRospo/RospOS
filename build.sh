2355cd918a88d532ea1752804c6d95368020abcc
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2026-02-02 11:29:36 -0800

    Start work on making beginnings of OS work

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euvo pipefail
 source rospoas/venv/bin/activate
-python3 rospoas/compile.py --input rospos/main.ros --output rospos/build/rospos.rosp
-hexdump -C rospos/build/rospos.rosp
+python3 rospoas/compile.py --input rospos/main.ros --output rospos/build/rospos.rosp 1>&2
+hexdump -C rospos/build/rospos.rosp 1>&2
 cd rospovm
-make -j8
+make -j8 1>&2
 cd ..
 ./rospovm/rospovm ./rospos/build/rospos.rosp --verbose
